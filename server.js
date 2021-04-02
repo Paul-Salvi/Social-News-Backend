578307fee33f8c0a10ec4e1e47329f95c270b9fe
Aniket Kapoor <aniketkapoor@live.in>
2021-04-03 00:30:22 +0530

    Creating a base file for integration with prisma

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express');
+const bodyParser=require('body-parser');
+const { PrismaClient } = require('@prisma/client');
+const prisma=new PrismaClient();
 const app = express();
-
 const port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.json());
 app.get('/', (req, res) => { res.send("{'Health Check':'Healthy' }"); });
+app.post('/createTags',async (req,res)=>{
+const {tag}=req.body;
+const post=await prisma.tags.create({
+data:{
+    name : tag
+}
+})
+res.json(post);
+})
+app.post('/getTags',async (req,res)=>{
+    const {tagNumber}=req.body;
+    const post=await prisma.tags.findFirst({
+    where:{
+        tagId : tagNumber,
+    },
+    })
+    res.json(post);
+    })
 app.listen(port, () => { console.log(`Server is listening on port ${port}`); });
