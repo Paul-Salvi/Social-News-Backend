53112b6909db6ca3018939e58c18a891f59e110e
Aniket Kapoor <aniketkapoor@live.in>
2021-04-13 22:58:31 +0530

    Creating the basic structure of the api

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
 const express = require('express');
-const bodyParser=require('body-parser');
-const { PrismaClient } = require('@prisma/client');
-const prisma=new PrismaClient();
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
-
+const productRouter=require( './src/routes/products.routes.js');
 app.use(bodyParser.json());
-app.get('/', (req, res) => { res.send("{'Health Check':'Healthy' }"); });
-app.post('/createTags',async (req,res)=>{
-const {tag}=req.body;
-const post=await prisma.tags.create({
-data:{
-    name : tag
-}
-})
-res.json(post);
-})
-app.post('/getTags',async (req,res)=>{
-    const {tagNumber}=req.body;
-    const post=await prisma.tags.findFirst({
-    where:{
-        tagId : tagNumber,
-    },
-    })
-    res.json(post);
-    })
+app.use('/api/v1/product',productRouter);
 app.listen(port, () => { console.log(`Server is listening on port ${port}`); });
